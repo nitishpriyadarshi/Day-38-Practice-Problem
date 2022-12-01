1c1d08ab8158ba199c809a675fb358d1b643697b
nitis <nitish8152@gmail.com>
2022-12-01 19:10:00 +0530

    Added:-Java Script Practice Problem

---
+++ Selection Practice Problems with case statements/ReadNumberAndDisplayUnit.js
@@ -0,0 +1,18 @@
+let number = 5;
+
+switch(number){
+    case 1:
+        console.log("One");
+        break;
+    case 2:
+        console.log("Ten");
+        break;   
+    case 3:
+        console.log("Hundred");
+        break;
+    case 4:
+        console.log("Thousand");
+        break;
+    default:
+        console.log("Invalid Input");
+}
